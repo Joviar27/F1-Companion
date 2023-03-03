57fc11025ab30e6092d1c62fb757edb4b7ae8e7b
Joviar27 <joviar27@gmail.com>
2023-03-04 01:28:34 +0700

    Create the team data class

--- app/src/main/java/com/example/f1companion/Team.kt
+++ app/src/main/java/com/example/f1companion/Team.kt
@@ -5,32 +5,34 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Team (
-    val name : String,
-    val thumbnailPic : String,
-    val shortOverview : String,
+    val name : String = "default",
+    val thumbnailPic : String = "default",
+    val shortOverview : String = "default",
 
-    val fullName: String,
-    val headerPic : String,
-    val bannerPic : String,
-    val longOverview : String,
+    val fullName: String = "default",
+    val headerPic : String = "default",
+    val bannerPic : String = "default",
+    val longOverview : String = "default",
 
-    val bioBase : String,
-    val bioTeamChief : String,
-    val bioTechnical : String,
-    val bioChassis : String,
-    val bioPU : String,
-    val bioEntry : String,
-    val bioWC : String,
-    val bioHighest : String,
-    val bioPoles : String,
-    val bioFL : String,
+    val bioBase : String = "default",
+    val bioTeamChief : String = "default",
+    val bioTechnical : String = "default",
+    val bioChassis : String = "default",
+    val bioPU : String = "default",
+    val bioEntry : String = "default",
+    val bioWC : String = "default",
+    val bioHighest : String = "default",
+    val bioPoles : String = "default",
+    val bioFL : String = "default",
 
-    val driver1Name : String,
-    val driver1Desc : String,
-    val driver2Name : String,
-    val driver2Desc : String,
+    val driver1Name : String = "default",
+    val driver1Desc : String = "default",
+    val driver1Pic : String = "default",
+    val driver2Name : String = "default",
+    val driver2Desc : String = "default",
+    val driver2Pic : String = "default",
 
-    val galleryPic1 : String,
-    val galleryPic2 : String,
-    val galleryPic3 : String
+    val galleryPic1 : String = "default",
+    val galleryPic2 : String = "default",
+    val galleryPic3 : String = "default"
     ) : Parcelable
