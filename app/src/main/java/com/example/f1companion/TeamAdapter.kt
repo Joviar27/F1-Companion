dc98f5354b043dfa6b18f181627fdea7d1daa70a
Joviar27 <joviar27@gmail.com>
2023-03-04 14:17:30 +0700

    Update adapter onCreate argument, add transition to glide

--- app/src/main/java/com/example/f1companion/TeamAdapter.kt
+++ app/src/main/java/com/example/f1companion/TeamAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.f1companion
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.f1companion.databinding.ViewholderBinding
 
 class TeamAdapter(
@@ -14,7 +18,7 @@ class TeamAdapter(
     inner class TeamViewHolder(var binding: ViewholderBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TeamViewHolder {
-        val binding = ViewholderBinding.inflate(LayoutInflater.from(parent.context))
+        val binding = ViewholderBinding.inflate(LayoutInflater.from(parent.context),parent,false)
         return TeamViewHolder(binding)
     }
 
@@ -26,6 +30,8 @@ class TeamAdapter(
         Glide.with(holder.itemView.context)
             .load(thumbnailPic)
             .placeholder(R.color.dusty_beige)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(holder.binding.ivThumbnail)
 
         holder.itemView.setOnClickListener {
